58ed83e56def9eeaabd894fcd81c509e6b15af78
ArdaCetin <57831340+ArdaCet@users.noreply.github.com>
2021-01-18 14:23:29 +0300

    Update Chip-seq_subsample_Mel.py

--- Python_Scripts/Chip-seq_subsample_Mel.py
+++ Python_Scripts/Chip-seq_subsample_Mel.py
@@ -1,22 +1,27 @@
 import pandas as pd
 import os
-import subprocess
 
+# All ChIP-seq files should be at the same directory.
+# Path where the created files will be.
 path = '/cta/users/ardacetin/globalRepair/MelanomaPrediction/chipseq/ChIP-seqProtocolFile_4hr/'
+# ChIP-seq file name labels, in this case they are SRR IDs.
+histones = ["SRR227397_SRR227398.fastq","SRR227579_SRR227580.fastq","SRR568344_SRR568345.fastq"]
 
-histones = ["SRR227397_SRR227398.fastq","SRR227579_SRR227580.fastq","SRR568344_SRR568345.fastq","SRR227501_SRR227502.fastq","SRR227596_SRR227597.fastq","SRR568397_SRR568398.fastq","SRR227554_SRR227555.fastq","SRR568266_SRR568267_SRR568268.fastq","SRR568399_SRR568400.fastq","SRR577321_SRR577322.fastq"]
+subsample_n = 0	       # The number of reads which will be randomly picked.
+Min_read_number = 0    # The minimum tolerated read number for a file for not involving in downsampling process.
 
 for histone in histones:
-	os.system("grep -c '^' " + path + histone + "/4_" + histone + "_edited_sorted.bed > " + path + histone + "/5_" + histone + "_counts.txt")
+	os.system("grep -c '^' " + path + histone + "/4_" + histone + "_edited_sorted.bed > " + path + histone + "/5_" + histone + "_counts.txt") # Find read numbers.
 	
 	fl = open(path + histone + "/5_" + histone + "_counts.txt","r")
 	output = int(fl.readline())
 	fl.close()
 	os.system("rm " + path + histone + "/5_" + histone + "_counts.txt")
-	if int(output) > 10349091:
+	if int(output) > Min_read_number:	# Check whether read number is higher than the given value. If so, downsample the corresponding file otherwise, skip it.
 		
 		data = pd.read_csv(path + histone + "/4_" + histone + "_edited_sorted.bed", sep="\t", index_col=False)
-		rdm = data.sample(n=2852000, random_state=1) #17573000
+		rdm = data.sample(n=subsample_n, random_state=1)
 		rdm.to_csv(path + histone + '/SPO_' + histone +'_3M_Melsubsampled.bed', sep="\t", index=False)
 		
+		# The created file will have undesired characters and spacing. The below line of code is advised to be run individually.
 		#os.system("sed 's/\"//g' " + path + histone + "/SPO_" + histone + "_12andhalfM_subsampled.bed > " + path + histone + "/SPO_" + histone + "_12andHalfM_subsampled.bed")
